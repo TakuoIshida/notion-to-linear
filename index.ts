c33c560114d52610e53d61c9a287a7d7a8ae68f4
TakuoIshida <archersparadox.100@gmail.com>
2025-06-10 23:52:08 +0900

    feat: enhance task migration logging and add error handling for environment variables
    - Added logging to indicate the number of tasks being migrated from Notion.
    - Updated the LinearService constructor to throw errors if required environment variables (LINEAR_API_KEY, LINEAR_TEAM_ID) are not set.
    - Introduced new methods in LinearService for fetching team member IDs by email and listing projects.
    - Improved comments for clarity on functionality related to team issues and project listings.

--- index.ts
+++ index.ts
@@ -10,6 +10,8 @@ async function main() {
 	const teamName = "cactus";
 	const teamTasks = await getNotionDatabase(notionDatabaseId, teamName);
 
+	console.log(`${teamTasks.results.length}件のタスクを移行します。`);
+
 	for (const page of teamTasks.results) {
 		if (!("properties" in page)) continue;
 		console.log("page.properties", page.properties);
@@ -21,14 +23,39 @@ async function main() {
 		const status = page.properties["ステータス"].status?.name || "";
 		const storyPoint = page.properties["story point"]?.number || 0;
 		console.log(
-			`title:${title} asigneeEmail:${asigneeEmail} status:${status} storyPoint:${storyPoint}`,
+			`移行します。→ title:${title} asigneeEmail:${asigneeEmail} status:${status} storyPoint:${storyPoint}`,
 		);
+
+		// const linearUser = await linear.getTeamMemberByEmail(asigneeEmail);
+		// console.log("linearUser", linearUser);
+
+		// await linear.createIssue({
+		// 	title: "移行するチケット",
+		// 	projectId: "df23595c7f5b",
+		// 	assigneeId: linearUser?.id,
+		// 	storyPoint: 1,
+		// 	description: "hogehoge",
+		// });
+		// console.log(`タイトル：${title} を移行しました。`);
+		// console.log("--------------------------------");
 	}
 
-	const teams = await linear.listTeams();
-	for (const team of teams.nodes) {
-		console.log(`Name: ${team.name}, ID: ${team.id}`);
+	// NOTE: チーム一覧を取得する
+	// const teams = await linear.listTeams();
+	// for (const team of teams.nodes) {
+	// 	console.log(`Name: ${team.name}, ID: ${team.id}`);
+	// }
+	// NOTE: チームのissue一覧を取得する
+	// const issues = await linear.listTeamIssues();
+	// for (const issue of issues.nodes) {
+	// 	console.log(`ID: ${issue.id}, Title: ${issue.title}`);
+	// }
+	// NOTE: チームのプロジェクト一覧を取得する
+	const projects = await linear.listProjects();
+	for (const project of projects.nodes) {
+		console.log(`ID: ${project.id}, Name: ${project.name}`);
 	}
+	console.log(`${teamTasks.results.length}件のタスクを移行しました。`);
 }
 
 main().catch(console.error);
--- linear/client.ts
+++ linear/client.ts
@@ -11,6 +11,12 @@ export class LinearService {
 	private teamId: string;
 
 	constructor() {
+		if (!process.env.LINEAR_API_KEY) {
+			throw new Error("LINEAR_API_KEY is not set");
+		}
+		if (!process.env.LINEAR_TEAM_ID) {
+			throw new Error("LINEAR_TEAM_ID is not set");
+		}
 		this.client = new LinearClient({ apiKey: process.env.LINEAR_API_KEY });
 		this.teamId = process.env.LINEAR_TEAM_ID ?? "";
 	}
@@ -22,6 +28,9 @@ export class LinearService {
 		return await this.client.teams();
 	}
 
+	/**
+	 * チームのissue一覧を取得する
+	 */
 	async listTeamIssues() {
 		return await this.client.issues({
 			filter: {
@@ -29,6 +38,49 @@ export class LinearService {
 					id: { eq: this.teamId },
 				},
 			},
+		});
+	}
+
+	/**
+	 * チームメンバーのIDを取得する
+	 * @param email メールアドレス
+	 * @returns チームメンバーのID
+	 */
+	async getTeamMemberByEmail(email: string) {
+		const user = await this.client.users({
+			filter: {
+				email: {
+					eq: email,
+				},
+			},
+			first: 1,
+		});
+		console.log("getTeamMemberByEmail", user);
+		return user.nodes[0];
+	}
+
+	async listProjects() {
+		return await this.client.projects({ first: 100 });
+	}
+
+	/**
+	 * タスクを作成する
+	 * @param input タスク作成の入力
+	 */
+	async createIssue(input: {
+		title: string;
+		projectId: string;
+		description: string;
+		assigneeId?: string;
+		storyPoint: number;
+	}) {
+		return await this.client.createIssue({
+			teamId: this.teamId,
+			title: input.title,
+			projectId: input.projectId,
+			description: input.description,
+			assigneeId: input.assigneeId,
+			estimate: input.storyPoint,
 		});
 	}
 }
